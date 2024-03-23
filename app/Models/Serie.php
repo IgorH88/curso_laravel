948f9355c24dda372d81d47533a37e2aa14c36e3
Igor Azevedo <igorhenrique307@gmail.com>
2024-03-23 17:54:23 -0300

    fix: adicionando relacionamento entre serie, temporada e episodio

--- app/Models/Serie.php
+++ app/Models/Serie.php
@@ -5,8 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+// use App\Models;
+
 class Serie extends Model
 {
     use HasFactory;
     protected $fillable = ['nome'];
+
+    public function temporadas()
+    {
+        return $this->hasMany(Season::class, 'series_id');
+    }
 }
