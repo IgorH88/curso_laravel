c90a954dc94217309593ac4704d1016fb100f1be
Igor Azevedo <igorhenrique307@gmail.com>
2024-05-29 09:56:04 -0300

    Adicionando provider

---
+++ app/Providers/SeriesRepositoryProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repository\EloquentSeriesRepository;
+use App\Repository\SeriesRepository;
+use Illuminate\Support\ServiceProvider;
+
+class SeriesRepositoryProvider extends ServiceProvider
+{
+    public array $bindings = [
+        SeriesRepository::class => EloquentSeriesRepository::class
+    ];
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
